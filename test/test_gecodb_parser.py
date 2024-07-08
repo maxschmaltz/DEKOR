4896a2a62b3475bbb8b31e5849b995e7be045da4
Max Schmaltz <schmaltzmax@gmail.com>
2024-07-08 23:40:00 +0200

    Update tests

--- test/test_gecodb_parser.py
+++ test/test_gecodb_parser.py
@@ -1,6 +1,6 @@
 import unittest
 
-from dekor.gecodb_parser import Compound, Stem, Link
+from dekor.utils.gecodb_parser import Compound, Stem, Link
 
 
 class TestGecoDBParser(unittest.TestCase):
@@ -99,6 +99,42 @@ class TestGecoDBParser(unittest.TestCase):
         expected_components = [
             Stem("bund", span=(0, 4)),
             Link("_+s_", realization="es", span=(4, 6), type="addition"),
+            Stem("verfassung", span=(6, 16)),
+            Link("_+s_", realization="s", span=(16, 17), type="addition"),
+            Stem("gericht", span=(17, 24))
+        ]
+        actual_components = compound.components
+        self.assertListEqual(expected_components, actual_components)
+        # compare pretty form
+        expected_lemma = "bundesverfassungsgericht"
+        actual_lemma = compound.lemma
+        self.assertEqual(expected_lemma, actual_lemma)
+
+    def test_addition_3(self):
+        # parse compound
+        raw = "nerv_+en_system"
+        compound = Compound(raw, eliminate_allomorphy=False)
+        # compare components and their properties
+        expected_components = [
+            Stem("nerv", span=(0, 4)),
+            Link("_+en_", realization="en", span=(4, 6), type="addition"),
+            Stem("system", span=(6, 12))
+        ]
+        actual_components = compound.components
+        self.assertListEqual(expected_components, actual_components)
+        # compare pretty form
+        expected_lemma = "nervensystem"
+        actual_lemma = compound.lemma
+        self.assertEqual(expected_lemma, actual_lemma)
+
+    def test_addition_4(self):
+        # parse compound
+        raw = "bund_+es_verfassung_+s_gericht"
+        compound = Compound(raw, eliminate_allomorphy=False)
+        # compare components and their properties
+        expected_components = [
+            Stem("bund", span=(0, 4)),
+            Link("_+es_", realization="es", span=(4, 6), type="addition"),
             Stem("verfassung", span=(6, 16)),
             Link("_+s_", realization="s", span=(16, 17), type="addition"),
             Stem("gericht", span=(17, 24))
@@ -263,6 +299,25 @@ class TestGecoDBParser(unittest.TestCase):
         expected_components = [
             Stem("bund", span=(0, 4)),
             Link("_+s_", realization="es", span=(4, 6), type="addition"),
+            Stem("arzt", realization="ärzt", span=(6, 10)),
+            Link("_+=e_", realization="e", span=(10, 11), type="addition_umlaut"),
+            Stem("kammer", span=(11, 17))
+        ]
+        actual_components = compound.components
+        self.assertListEqual(expected_components, actual_components)
+        # compare pretty form
+        expected_lemma = "bundesärztekammer"
+        actual_lemma = compound.lemma
+        self.assertEqual(expected_lemma, actual_lemma)
+
+    def test_complex_compound_3(self):
+        # parse compound
+        raw = "bund_+es_arzt_+=e_kammer"
+        compound = Compound(raw, eliminate_allomorphy=False)
+        # compare components and their properties
+        expected_components = [
+            Stem("bund", span=(0, 4)),
+            Link("_+es_", realization="es", span=(4, 6), type="addition"),
             Stem("arzt", realization="ärzt", span=(6, 10)),
             Link("_+=e_", realization="e", span=(10, 11), type="addition_umlaut"),
             Stem("kammer", span=(11, 17))
--- test/test_models.py
+++ test/test_models.py
@@ -1,8 +1,10 @@
 import unittest
+import re
 from sklearn.model_selection import train_test_split
 
-from dekor.gecodb_parser import parse_gecodb
-from dekor.models.ngrams import NGramsSplitter
+from dekor.utils.gecodb_parser import parse_gecodb, UMLAUTS_REVERSED
+from dekor.splitters import NGramsSplitter, RNNSplitter
+from dekor.benchmarking.benchmarking import eval_splitter
 
 
 class TestNGramsSplitter(unittest.TestCase):
@@ -14,26 +16,86 @@ class TestNGramsSplitter(unittest.TestCase):
     splits them.
     """
 
-    def test_lemma_completeness(self):
+    def test_lemmas_correctness(self):
         gecodb_path = "./resources/gecodb_v04.tsv"
-        gecodb = parse_gecodb(gecodb_path, min_count=10000)
-        train_data, test_data = train_test_split(gecodb, train_size=0.75, shuffle=False)
-        train_compounds = train_data["compound"].values
-        test_compounds = test_data["compound"].values
-        test_lemmas = [
-            compound.lemma for compound in test_compounds
-        ]
-        splitter = NGramsSplitter(
-            n=2,
-            record_none_links=False,
-            verbose=False
-        ).fit(train_compounds)
+        for eliminate_allomorphy in [True, False]:
+            gecodb = parse_gecodb(
+                gecodb_path,
+                eliminate_allomorphy=eliminate_allomorphy,
+                min_count=10000
+            )
+            train_data, test_data = train_test_split(gecodb, train_size=0.75, shuffle=True)
+            train_compounds = train_data["compound"].values
+            test_compounds = test_data["compound"].values
+            for n in [2, 3, 4]:
+                unfit_splitter = NGramsSplitter(
+                    n=n,
+                    record_none_links=False,
+                    eliminate_allomorphy=eliminate_allomorphy,
+                    verbose=False
+                )
+                _, pred_compounds = eval_splitter(
+                    unfit_splitter=unfit_splitter,
+                    train_compounds=train_compounds,
+                    test_compounds=test_compounds
+                )
+                test_lemmas = [
+                    compound.lemma for compound in test_compounds
+                ]
+                pred_lemmas = [
+                    compound.lemma for compound in pred_compounds
+                ]
+                self.assertListEqual(test_lemmas, pred_lemmas)
 
-        pred_compounds = splitter.predict(test_lemmas)
-        pred_lemmas = [
-            compound.lemma for compound in pred_compounds
-        ]
-        self.assertListEqual(test_lemmas, pred_lemmas)
+
+class TestRNNSplitter(unittest.TestCase):
+
+    """
+    In principle, the only thing we can test about the model
+    is it's adequacy in the sense that it does not remove
+    anything from or add anything from the compounds however it
+    splits them.
+    """
+
+    def test_lemmas_correctness(self):
+        gecodb_path = "./resources/gecodb_v04.tsv"
+        for eliminate_allomorphy in [True, False]:
+            gecodb = parse_gecodb(gecodb_path, eliminate_allomorphy=eliminate_allomorphy, min_count=10000)
+            train_data, test_data = train_test_split(gecodb, train_size=0.75, shuffle=True)
+            train_compounds = train_data["compound"].values
+            test_compounds = test_data["compound"].values
+            test_lemmas = [
+                compound.lemma for compound in test_compounds
+            ]
+            for n in [2, 3, 4]:
+                unfit_splitter = RNNSplitter(
+                    n=n,
+                    eliminate_allomorphy=eliminate_allomorphy,
+                    batch_size=4096,
+                    verbose=False
+                )
+                _, pred_compounds = eval_splitter(
+                    unfit_splitter=unfit_splitter,
+                    train_compounds=train_compounds,
+                    test_compounds=test_compounds
+                )
+                test_lemmas = [
+                    compound.lemma for compound in test_compounds
+                ]
+                pred_lemmas = [
+                    compound.lemma for compound in pred_compounds
+                ]
+                # here, the model might predict umlaut in positions
+                # where this is not the case, so we'll need
+                # to remove all umlauts to lay off the effect
+                test_lemmas_before, pred_lemmas_before = [], []
+                for test_lemma, pred_lemma in zip(test_lemmas, pred_lemmas):
+                    for after, before in UMLAUTS_REVERSED.items():
+                        test_lemma = test_lemma.replace(after, before)
+                        pred_lemma = pred_lemma.replace(after, before)
+                    test_lemmas_before.append(test_lemma)
+                    pred_lemmas_before.append(pred_lemma)
+                self.assertListEqual(test_lemmas_before, pred_lemmas_before)
 
 
 if __name__ == '__main__':
