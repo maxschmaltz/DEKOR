e72b8b186e670115fd99b8dda4635558b0e95ff0
Max Schmaltz <schmaltzmax@gmail.com>
2024-07-22 12:37:07 +0200

    Some more docs and comments

--- dekor/splitters/__init__.py
+++ dekor/splitters/__init__.py
@@ -1,2 +1,12 @@
+"""
+Base model for splitting German compounds based on the DECOW16 compound data.
+"""
+
 from dekor.splitters.ngrams import NGramsSplitter
-from dekor.splitters.rnn import RNNSplitter
+from dekor.splitters.nns import FFNSplitter, RNNSplitter
+
+__all__ = {
+    NGramsSplitter.name: NGramsSplitter,
+    FFNSplitter.name: FFNSplitter,
+    RNNSplitter.name: RNNSplitter
+}
--- dekor/utils/datasets.py
+++ dekor/utils/datasets.py
@@ -1,9 +1,25 @@
+"""
+Module with custom dataset formats.
+"""
+
 from torch.utils.data import Dataset
 
 from typing import Optional, Iterable, Any
 
 
 class XYDataset(Dataset):
+
+    """
+    Simple dataset for batching.
+
+    Parameters:
+    X : `Iterable[Any]`
+        train sequence
+
+    Y : `Iterable[Any]`, optional
+        target sequence; optional, if not provided, the dataset will return
+        only train subsequences upon iteration
+    """
 
     def __init__(self, X: Iterable[Any], Y: Optional[Iterable[Any]]=None) -> None:
         self.X = X
