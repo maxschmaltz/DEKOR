2607bfb66b875c210cc9ad4e4323d42684e81ccd
Max Schmaltz <schmaltzmax@gmail.com>
2024-07-22 12:35:33 +0200

    Add docs and comments

--- dekor/benchmarking/benchmarking.py
+++ dekor/benchmarking/benchmarking.py
@@ -2,20 +2,44 @@ import os
 import json
 from itertools import product
 from sklearn.model_selection import train_test_split
-from typing import Optional, List, Dict
+from io import BytesIO
+from PIL import Image
+from PIL.PngImagePlugin import PngInfo
+from typing import Optional, List, Dict, Iterable
 
-from dekor.splitters.base import BaseSplitter
 import dekor.splitters
-from dekor.utils.gecodb_parser import parse_gecodb, Compound, Tuple
-from dekor.eval.evaluator import CompoundEvaluator
+from dekor.splitters.base import BaseSplitter
+from dekor.utils.gecodb_parser import parse_gecodb, Compound, Tuple, Union
+from dekor.eval.evaluator import CompoundEvaluator, EvaluationResult
 
 evaluator = CompoundEvaluator()
 
 
 def predict_with_splitter(
     splitter: BaseSplitter,
-    test_compounds: List[Compound]
-):
+    test_compounds: Iterable[Compound]
+) -> Tuple[List[Compound], EvaluationResult]:
+    
+    """
+    Predict lemmas with a splitter.
+
+    Parameters
+    ----------
+    splitter : `BaseSplitter`
+        fit splitter
+
+    test_compounds : `Iterable[Compound]`
+        compounds to predict; note that it is not an `Iterable[str]`
+        as in the splitters because this method is used for evaluation,
+        not inference
+
+    Returns:
+    `List[Compound]`
+        preds
+    `EvaluationResult`
+        the result of evaluation
+    """
+
     test_lemmas = [
         compound.lemma for compound in test_compounds
     ]
@@ -26,11 +50,30 @@ def predict_with_splitter(
 
 def eval_splitter(
     *,
-    unfit_splitter: BaseSplitter,
-    train_compounds: List[Compound],
+    splitter: BaseSplitter,
     test_compounds: List[Compound]
-):
-    splitter = unfit_splitter.fit(train_compounds)
+) -> Tuple[EvaluationResult, List[Compound]]:
+    
+    """
+    Evaluate splitter on test compounds.
+
+    Parameters
+    ----------
+    splitter : `BaseSplitter`
+        fit splitter
+
+    test_compounds : `Iterable[Compound]`
+        compounds to predict; note that it is not an `Iterable[str]`
+        as in the splitters because this method is used for evaluation,
+        not inference
+
+    Returns:
+    `EvaluationResult`
+        the result of evaluation
+    `List[Compound]`
+        preds
+    """
+
     pred_compounds, scores = predict_with_splitter(splitter, test_compounds)
     return scores, pred_compounds
 
@@ -43,12 +86,40 @@ def benchmark_splitter(
     gecodb_path: str,
     train_size: Optional[float]=0.8,
     verbose: Optional[bool]=True
-):
+) -> Tuple[List[dict], Tuple[List[Compound], List[Compound]], Union[BytesIO, None]]:
+    
+    """
+    Benchmark a single splitter given different configurations.
+
+    Parameters
+    ----------
+    splitter_cls : `type`
+        splitter class
+
+    min_counts : `List[int]`:
+        minimal counts of compounds to keep;
+        all compounds occurring less will be dropped at the respective iteration
+
+    all_splitter_params : `List[dict]`
+        list of tested configurations for the splitter,
+        with each configuration being a mapping between parameter name and its value
+
+    gecodb_path : `str`
+        path to the TSV DECOW16-format compounds dataset
+
+    train_size : `float`, optional, defaults to `0.8`
+        train size
+
+    verbose : `bool` optional, defaults to `True`
+        whether to show progress bar when fitting and predicting compounds
+    """
+
     outputs = []
-    best_score = -1
+    best_score = -1 # so that even 0 is instantly better
     best_pairs = None
-    eliminate_allomorphys = [True, False]
-    n_iter = 1
+    best_plot_buffer = None
+    eliminate_allomorphys = [True, False]   # necessarily test both
+    n_iter = 1  # for printout
     for min_count, eliminate_allomorphy in product(min_counts, eliminate_allomorphys):
         gecodb = parse_gecodb(
             gecodb_path,
@@ -59,17 +130,16 @@ def benchmark_splitter(
         train_compounds = train_data["compound"].values
         test_compounds = test_data["compound"].values
         for splitter_params in all_splitter_params:
-            unfit_splitter = splitter_cls(
+            splitter = splitter_cls(
                 **splitter_params,
                 eliminate_allomorphy=eliminate_allomorphy,
                 verbose=verbose
-            )
+            ).fit(train_compounds)
             if verbose: # print out params
                 print('\n', f"{splitter_cls.name}: iter {n_iter}/{len(all_splitter_params * len(min_counts) * 2)}")
                 print(', '.join(f"{key}: {value}" for key, value in splitter_params.items()), f"min_count: {min_count}")
             scores, pred_compounds = eval_splitter(
-                unfit_splitter=unfit_splitter,
-                train_compounds=train_compounds,
+                splitter=splitter,
                 test_compounds=test_compounds
             )
             mean_score = scores.mean()
@@ -79,15 +149,21 @@ def benchmark_splitter(
                 "train_size": len(train_compounds),
                 "test_size": len(test_compounds),
                 "splitter_name": splitter_cls.name,
-                "splitter_metadata": unfit_splitter._metadata()
+                "splitter_metadata": splitter._metadata()
             }
             outputs.append(output)
             if mean_score > best_score:
                 best_score = mean_score
                 best_pairs = (test_compounds, pred_compounds)
+                if hasattr(splitter, "plot_buffer") and splitter.plot_buffer is not None:
+                    best_plot_buffer = splitter.plot_buffer
+            else:
+                if hasattr(splitter, "plot_buffer") and splitter.plot_buffer is not None:
+                    splitter.plot_buffer.truncate(0)
+                    splitter.plot_buffer.close()
             n_iter += 1
     outputs = sorted(outputs, key=lambda entry: entry["mean_score"], reverse=True)
-    return outputs, best_pairs
+    return outputs, best_pairs, best_plot_buffer
 
 
 def benchmark_splitters(
@@ -98,16 +174,64 @@ def benchmark_splitters(
     main_suffix: Optional[str]="",
     train_size: Optional[float]=0.8,
     verbose: Optional[bool]=True
-):
+) -> None:
+    
+    """
+    Benchmark one or more splitters given their different configurations.
+
+    Parameters
+    ----------
+    cls2params : `Dict[str, Tuple[List[int], List[dict], str]]`
+        mapping between name of the splitter (according to its .name attribute)
+        and tuple out of 3 elements:
+            1. `List[int]`: minimal counts of compounds to keep;
+            all compounds occurring less will be dropped at the respective iteration
+            2. `List[dict]`: list of tested configurations for the splitter,
+            with each configuration being a mapping between parameter name and its value
+            3. `str`: suffix to prepend to this model out directory (<class_name_x_suffix> below)
+
+    gecodb_path : `str`
+        path to the TSV DECOW16-format compounds dataset
+
+    out_dir : `str`
+        path to directory to put the results to
+
+    main_suffix : `str`, optional
+        suffix to append to all output files before the class suffices
+
+    train_size, optional, defaults to `0.8`
+        train size
+
+    verbose : `bool` optional, defaults to `True`
+        whether to show progress bar when fitting and predicting compounds
+
+    Notes
+    -----
+    The result of evaluation will be written to `out_dir` with the following structure:
+        out_dir
+        --- <name of the class_name_1 object>
+            --- scores<--suffix><class_name_1_suffix>.json: JSON with sorted scores for the class_name_1 model
+            --- best_preds<--suffix><class_name_1_suffix>.tsv:
+                TSV with columns of golds and preds from the best config of the class_name_1 model
+            --- best_plot<--suffix><class_name_1_suffix>.png: (if given) training plot
+                from the best config of the class_name_1 model
+        --- <name of the class_name_2 object>
+            ...
+        --- ...
+            ...
+        all_scores<--suffix>.json: JSON with all sorted scores
+        best_preds<--suffix>.tsv: TSV with columns of golds and preds from the best config of the best model
+    """
+
     all_outputs = []
     best_pair_of_all = None
     best_score = -1
     if not os.path.exists(out_dir): os.mkdir(out_dir)
     for splitter_cls_name, (min_counts, all_splitter_params, suffix) in cls2params.items():
-        splitter_cls = getattr(dekor.splitters, splitter_cls_name)
+        splitter_cls = dekor.splitters.__all__[splitter_cls_name]
         target_dir = os.path.join(out_dir, splitter_cls.name)
         if not os.path.exists(target_dir): os.mkdir(target_dir)
-        outputs, best_pairs = benchmark_splitter(
+        outputs, best_pairs, best_plot_buffer = benchmark_splitter(
             splitter_cls=splitter_cls,
             min_counts=min_counts,
             all_splitter_params=all_splitter_params,
@@ -125,6 +249,13 @@ def benchmark_splitters(
                 f"{gold.raw}\t{pred.raw}\n"
                 for gold, pred in zip(*best_pairs)
             ])
+        if best_plot_buffer:
+            best_plot_path = os.path.join(target_dir, f"best_plot{main_suffix}{suffix}.png")
+            best_plot_buffer.seek(0)
+            plot = Image.open(best_plot_buffer)
+            info = PngInfo()
+            for key, value in plot.text.items(): info.add_text(key, value)
+            plot.save(best_plot_path, format="png", pnginfo=info)
         if outputs[0]["mean_score"] > best_score:
             best_score = outputs[0]["mean_score"]
             best_pair_of_all = best_pairs
--- dekor/benchmarking/main.py
+++ dekor/benchmarking/main.py
@@ -13,12 +13,15 @@ def grid_parameters(parameters: Dict[str, List[Any]]) -> List[Dict[str, Any]]:
     ]
 
 
-def main():
+def main() -> None:
 
-    parser = argparse.ArgumentParser()
+    """
+    Run benchmarking of splitters. CLI-run.
 
-    parser.add_argument(
-        "params_mapping_path", help="""Path to a JSON with of following structure:
+    CLI Parameters
+    --------------
+    params_mapping_path : `str`
+        path to a JSON with of following structure:
         {
             class_name_1: [
                 [min_count_1, min_count_2, ...],
@@ -32,13 +35,72 @@ def main():
             class_name_2: ...,
             ...
         }
+        where min_count_x is the minimum count for parsing GecoDB,
+        param_x is a list of values for param_x; all param values for all params
+        will be combined, so all the configurations will be tested. Note
+        that both variant with eliminating allomorphy and not will be run
+
+    gecodb_path : `str`
+        path to GecoDB compound dataset
+
+    out_dir : `str`
+        path to directory to put the results to
+
+    --suffix, -s : `str`, optional
+        suffix to append to all output files before the class suffices
+
+    --train_size", "-ts", `float`, optional, defaults to `0.8`
+        train size
+
+    -q, optional, defaults to `False`,
+        whether to suppress verbose; `True` if given, `False` if missing
+
+    Notes
+    -----
+    The result of evaluation will be written to `out_dir` with the following structure:
+        out_dir
+        --- <name of the class_name_1 object>
+            --- scores<--suffix><class_name_1_suffix>.json: JSON with sorted scores for the class_name_1 model
+            --- best_preds<--suffix><class_name_1_suffix>.tsv:
+                TSV with columns of golds and preds from the best config of the class_name_1 model
+            --- best_plot<--suffix><class_name_1_suffix>.png: (if given) training plot
+                from the best config of the class_name_1 model
+        --- <name of the class_name_2 object>
+            ...
+        --- ...
+            ...
+        all_scores<--suffix>.json: JSON with all sorted scores
+        best_preds<--suffix>.tsv: TSV with columns of golds and preds from the best config of the best model
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "params_mapping_path", help="""path to a JSON with of following structure:
+        {
+            class_name_1: [
+                [min_count_1, min_count_2, ...],
+                {
+                    param_1: [param_1_val_1, param_1_val_2, ...],
+                    param_2: [param_2_val_1, param_2_val_2, ...],
+                    ...
+                },
+                class_name_1_suffix
+            ],
+            class_name_2: ...,
+            ...
+        }
+        where min_count_x is the minimum count for parsing GecoDB,
+        param_x is a list of values for param_x; all param values for all params
+        will be combined, so all the configurations will be tested. Note
+        that both variant with eliminating allomorphy and not will be run
         """
     )
     parser.add_argument("gecodb_path", help="path to GecoDB compound dataset")
     parser.add_argument("out_dir", help="path to directory to put the results to")
-    parser.add_argument("--suffix", "-s", help="suffix to append to all output files before the class suffices")
+    parser.add_argument("--suffix", "-s", default="", help="suffix to append to all output files before the class suffices")
     parser.add_argument("--train_size", "-ts", type=float, default=0.8, help="train size")
-    parser.add_argument("-q", action="store_false", help="suppress verbose")
+    parser.add_argument("-q", action="store_false", default=False, help="whether to suppress verbose")
 
     args = parser.parse_args()
 
@@ -46,7 +108,7 @@ def main():
     cls2params = {
         key: [
             min_counts,
-            grid_parameters(params_options),
+            grid_parameters(params_options),    # combine param values
             suffix
         ]
         for key, (min_counts, params_options, suffix) in params_mapping.items()
