51ff7c84c0c3081c60d9151b207492cb765b5e0b
Max Schmaltz <schmaltzmax@gmail.com>
2024-09-11 19:05:18 +0200

    add GRU and CNN to all

--- dekor/splitters/__init__.py
+++ dekor/splitters/__init__.py
@@ -3,10 +3,12 @@ Base model for splitting German compounds based on the DECOW16 compound data.
 """
 
 from dekor.splitters.ngrams import NGramsSplitter
-from dekor.splitters.nns import FFNSplitter, RNNSplitter
+from dekor.splitters.nns import FFNSplitter, RNNSplitter, GRUSplitter, CNNSplitter
 
 __all__ = {
     NGramsSplitter.name: NGramsSplitter,
     FFNSplitter.name: FFNSplitter,
-    RNNSplitter.name: RNNSplitter
+    RNNSplitter.name: RNNSplitter,
+	GRUSplitter.name: GRUSplitter,
+	CNNSplitter.name: CNNSplitter
 }
