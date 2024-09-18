b13de59e21bef9621b2d1dc486c0e9e2e3eec74f
Max Schmaltz <schmaltzmax@gmail.com>
2024-09-18 12:25:55 +0200

    finalize `TorchEmbeddings`

--- dekor/embeddings/base.py
+++ dekor/embeddings/base.py
@@ -1,15 +1,23 @@
 from abc import ABC, abstractmethod
 import asyncio
 import torch
-from typing import Iterable, Awaitable, Optional
-
-from dekor.utils.vocabs import StringVocab
+from typing import Iterable, Awaitable
 
 
 class BaseEmbeddings(ABC):
 
 	name: str
-	vocab: Optional[StringVocab]=None
+	requires_vocab: bool
+	trainable: bool
+	_embedding_dim: int
+
+	@abstractmethod
+	def train(self) -> None:
+		pass
+
+	@abstractmethod
+	def eval(self) -> None:
+		pass
 
 	@abstractmethod
 	async def aembed_single(self, text: str, *args, **kwargs) -> Awaitable[torch.Tensor]:
@@ -18,13 +26,9 @@ class BaseEmbeddings(ABC):
 	async def aembed(self, texts: Iterable[str], *args, **kwargs) -> torch.Tensor:
 		tasks = [self.aembed_single(text, *args, **kwargs) for text in texts]
 		embeddings = await asyncio.gather(*tasks)
+		embeddings = torch.stack(embeddings, dim=0)
 		return embeddings
 
 	def embed(self, texts: Iterable[str], *args, **kwargs) -> torch.Tensor:
 		embeddings = asyncio.run(self.aembed(texts, *args, **kwargs))
-		return embeddings
-	
-	@abstractmethod
-	@property
-	def _embedding_dim(self) -> int:
-		pass
+		return embeddings	# b x emb
--- dekor/embeddings/torch_embeddings.py
+++ dekor/embeddings/torch_embeddings.py
@@ -1,40 +1,51 @@
-import asyncio
-import numpy as np
 import torch
 import torch.nn as nn
-from typing import Iterable, Awaitable, Tuple
+from typing import Awaitable
 
 from dekor.splitters.base import DEVICE
 from dekor.embeddings.base import BaseEmbeddings
 from dekor.utils.vocabs import StringVocab
 
 
-class TorchEmbeddings(BaseEmbeddings, nn.Module):
+class TorchEmbeddings(nn.Module, BaseEmbeddings):
 
 	name = "torch"
+	requires_vocab = True
+	trainable = True
 
 	def __init__(
 		self,
-		*
+		*,
 		vocab: StringVocab,
 		embedding_dim: int
 	) -> None:
 		super(TorchEmbeddings, self).__init__()
 		self.vocab = vocab
 		self.embedding_dim = embedding_dim
-		self.undelying_embedding = nn.Embedding(
+		self.underlying_embeddings = nn.Embedding(
 			num_embeddings=len(vocab),
 			embedding_dim=embedding_dim
 		)
-	
-	async def aembed_single(self, text: str, *args, **kwargs) -> Awaitable[torch.Tensor]:
-		raise NotImplementedError("Torch embeddings implements `embed` function directly")
 
-	async def aembed(self, texts: Iterable[str], *args, **kwargs) -> torch.Tensor:
-		raise NotImplementedError("Torch embeddings implements `embed` function directly")
+	# `train()` and `eval()` are implemented in `nn.Module`
 	
-	def embed(self, texts: Iterable[str]) -> torch.Tensor:
-		pass	# use `forward()`	!!!
+	async def aembed_single(self, text: str) -> Awaitable[torch.Tensor]:
+		# Even though in torch, we can operate over whole tensors fast and efficiently,
+		# that requires that we have the same length of texts every time, hence, padding.
+		# The texts are already too short and it might be too noisy to add padding;
+		# That is why we will better embed each text separately and just ensure the same
+		# shape of the output.
+		# Our implementation here is character-wise; to get the most significant
+		# embedding values from different characters, we use max pooling;
+		# in cases like RNN it won't affect anything because RNN input
+		# is already character-wise. This will also ensure the same output shape
+		# no matter what the input text length is.
+		text = text or ['']	# '' is also added manually
+		char_ids = [self.vocab.encode(char) for char in text]	# l
+		char_ids = torch.tensor(char_ids, dtype=torch.long, device=DEVICE)	# l
+		char_embeddings = self.underlying_embeddings(char_ids)	# l x emb
+		text_embedding, _ = torch.max(char_embeddings, dim=0)	# emb
+		return text_embedding
 	
 	@property
 	def _embedding_dim(self) -> int:
