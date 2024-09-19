a1d9f14e858e506ecaf205586b05ba85dce3f2eb
Max Schmaltz <schmaltzmax@gmail.com>
2024-09-19 18:43:35 +0200

    add test for GBERT splitter

--- test/test_models.py
+++ test/test_models.py
@@ -7,7 +7,8 @@ from dekor.splitters import (
     FFNSplitter,
     RNNSplitter,
     GRUSplitter,
-    CNNSplitter
+    CNNSplitter,
+	GBERTSplitter
 )
 from dekor.benchmarking.benchmarking import eval_splitter
 
@@ -19,12 +20,18 @@ class TestLemmaCorrectness(unittest.TestCase):
     # anything from or add anything from the compounds however it
     # splits them.
 
+	# also, many parameters are shared between the models
+	# (like `context_window` or embeddings in NN-based models),
+	# so we distributed different values of share parameters
+	# between different models in order to test more of them
+	# more efficiently
+
 	gecodb_path = "./resources/gecodb_v04.tsv"
 
 	def get_data(self):
 		gecodb = parse_gecodb(
 			self.gecodb_path,
-			min_count=10000
+			min_count=100000
 		)
 		train_data, test_data = train_test_split(gecodb, train_size=0.75, shuffle=True)
 		train_compounds = train_data["compound"].values
@@ -36,144 +43,155 @@ class TestLemmaCorrectness(unittest.TestCase):
 		test_lemmas = [
 			compound.lemma for compound in test_compounds
 		]
-		for n in [2, 3, 4]:
-			splitter = NGramsSplitter(
-				n=n,
-				record_none_links=False,
-				verbose=False
-			).fit(train_compounds)
-			_, pred_compounds = eval_splitter(
-				splitter=splitter,
-				test_compounds=test_compounds
-			)
-			pred_lemmas = [
-				compound.lemma for compound in pred_compounds
-			]
-			self.assertListEqual(test_lemmas, pred_lemmas)
+		splitter = NGramsSplitter(
+			n=3,
+			record_none_links=False,
+			verbose=False
+		).fit(train_compounds=train_compounds, test=True)
+		_, pred_compounds = eval_splitter(
+			splitter=splitter,
+			test_compounds=test_compounds
+		)
+		pred_lemmas = [
+			compound.lemma for compound in pred_compounds
+		]
+		self.assertListEqual(test_lemmas, pred_lemmas)
         
 	def test_ffn(self):
 		train_compounds, test_compounds = self.get_data()
 		test_lemmas = [
 			compound.lemma for compound in test_compounds
 		]
-		for n in [2, 3, 4]:
-			splitter = FFNSplitter(
-				context_window=n,
-				record_none_links=False,
-				embeddings_params={
-					"name": "torch",
-					"embedding_dim": 16
-				},
-				nn_params={
-					"hidden_size": 128,
-					"activation": "relu",
-					"dropout_rate": 0.025
-				},
-				batch_size=4096,
-				verbose=False
-			).fit(train_compounds)
-			_, pred_compounds = eval_splitter(
-				splitter=splitter,
-				test_compounds=test_compounds
-			)
-			pred_lemmas = [
-				compound.lemma for compound in pred_compounds
-			]
-			self.assertListEqual(test_lemmas, pred_lemmas)
+		splitter = FFNSplitter(
+			context_window=4,
+			record_none_links=False,
+			embeddings_params={
+				"name": "flair"
+			},
+			nn_params={
+				"hidden_size": 128,
+				"activation": "relu",
+				"dropout_rate": 0.025
+			},
+			batch_size=4096,
+			verbose=False
+		).fit(train_compounds=train_compounds, test=True)
+		_, pred_compounds = eval_splitter(
+			splitter=splitter,
+			test_compounds=test_compounds
+		)
+		pred_lemmas = [
+			compound.lemma for compound in pred_compounds
+		]
+		self.assertListEqual(test_lemmas, pred_lemmas)
 
 	def test_rnn(self):
 		train_compounds, test_compounds = self.get_data()
 		test_lemmas = [
 			compound.lemma for compound in test_compounds
 		]
-		for n in [2, 3, 4]:
-			splitter = RNNSplitter(
-				context_window=n,
-				record_none_links=False,
-				embeddings_params={
-					"name": "torch",
-					"embedding_dim": 16
-				},
-				nn_params={
-					"hidden_size": 128,
-					"activation": "relu",
-					"dropout_rate": 0.025,
-					"bidirectional": False,
-					"num_layers": 2
-				},
-				batch_size=4096,
-				verbose=False
-			).fit(train_compounds)
-			_, pred_compounds = eval_splitter(
-				splitter=splitter,
-				test_compounds=test_compounds
-			)
-			pred_lemmas = [
-				compound.lemma for compound in pred_compounds
-			]
-			self.assertListEqual(test_lemmas, pred_lemmas)
+		splitter = RNNSplitter(
+			context_window=5,
+			record_none_links=False,
+			embeddings_params={
+				"name": "torch",
+				"embedding_dim": 16
+			},
+			nn_params={
+				"hidden_size": 128,
+				"activation": "relu",
+				"dropout_rate": 0.025,
+				"num_layers": 2
+			},
+			batch_size=4096,
+			verbose=False
+		).fit(train_compounds=train_compounds, test=True)
+		_, pred_compounds = eval_splitter(
+			splitter=splitter,
+			test_compounds=test_compounds
+		)
+		pred_lemmas = [
+			compound.lemma for compound in pred_compounds
+		]
+		self.assertListEqual(test_lemmas, pred_lemmas)
 
 	def test_gru(self):
 		train_compounds, test_compounds = self.get_data()
 		test_lemmas = [
 			compound.lemma for compound in test_compounds
 		]
-		for n in [2, 3, 4]:
-			splitter = GRUSplitter(
-				context_window=n,
-				record_none_links=False,
-				embeddings_params={
-					"name": "torch",
-					"embedding_dim": 16
-				},
-				nn_params={
-					"hidden_size": 128,
-					"dropout_rate": 0.025,
-					"bidirectional": False,
-					"num_layers": 2
-				},
-				batch_size=4096,
-				verbose=False
-			).fit(train_compounds)
-			_, pred_compounds = eval_splitter(
-				splitter=splitter,
-				test_compounds=test_compounds
-			)
-			pred_lemmas = [
-				compound.lemma for compound in pred_compounds
-			]
-			self.assertListEqual(test_lemmas, pred_lemmas)
+		splitter = GRUSplitter(
+			context_window=6,
+			record_none_links=False,
+			embeddings_params={
+				"name": "torch",
+				"embedding_dim": 16
+			},
+			nn_params={
+				"hidden_size": 128,
+				"dropout_rate": 0.025,
+				"num_layers": 2
+			},
+			batch_size=4096,
+			verbose=False
+		).fit(train_compounds=train_compounds, test=True)
+		_, pred_compounds = eval_splitter(
+			splitter=splitter,
+			test_compounds=test_compounds
+		)
+		pred_lemmas = [
+			compound.lemma for compound in pred_compounds
+		]
+		self.assertListEqual(test_lemmas, pred_lemmas)
 
 	def test_cnn(self):
 		train_compounds, test_compounds = self.get_data()
 		test_lemmas = [
 			compound.lemma for compound in test_compounds
 		]
-		for n in [2, 3, 4]:
-			splitter = CNNSplitter(
-				context_window=n,
-				record_none_links=False,
-				embeddings_params={
-					"name": "torch",
-					"embedding_dim": 16
-				},
-				nn_params={
-					"hidden_size": 128,
-					"activation": "relu",
-					"reduction": "conv",
-					"dropout_rate": 0.025
-				},
-				batch_size=4096,
-				verbose=False
-			).fit(train_compounds)
-			_, pred_compounds = eval_splitter(
-				splitter=splitter,
-				test_compounds=test_compounds
-			)
-			pred_lemmas = [
-				compound.lemma for compound in pred_compounds
-			]
-			self.assertListEqual(test_lemmas, pred_lemmas)
+		splitter = CNNSplitter(
+			context_window=7,
+			record_none_links=False,
+			embeddings_params={
+				"name": "flair"
+			},
+			nn_params={
+				"hidden_size": 128,
+				"activation": "relu",
+				"reduction": "conv",
+				"dropout_rate": 0.025
+			},
+			batch_size=4096,
+			verbose=False
+		).fit(train_compounds=train_compounds, test=True)
+		_, pred_compounds = eval_splitter(
+			splitter=splitter,
+			test_compounds=test_compounds
+		)
+		pred_lemmas = [
+			compound.lemma for compound in pred_compounds
+		]
+		self.assertListEqual(test_lemmas, pred_lemmas)
+
+	def test_gbert(self):
+		train_compounds, test_compounds = self.get_data()
+		test_lemmas = [
+			compound.lemma for compound in test_compounds
+		]
+		splitter = GBERTSplitter(
+			context_window=10,
+			record_none_links=False,
+			batch_size=64,
+			verbose=False
+		).fit(train_compounds=train_compounds, test=True)
+		_, pred_compounds = eval_splitter(
+			splitter=splitter,
+			test_compounds=test_compounds
+		)
+		pred_lemmas = [
+			compound.lemma for compound in pred_compounds
+		]
+		self.assertListEqual(test_lemmas, pred_lemmas)
 
 
 if __name__ == '__main__':
